11bdd95674d89354db1bf17163a3f27ac837d385
fzzbt <juhanipm@gmail.com>
2011-10-20 15:31:42 +0300

    setup changes

--- setup.py
+++ setup.py
@@ -5,15 +5,15 @@ try:
 except ImportError:
     from distutils.core import setup
 
-VERSION = '0.0.1'
+VERSION = '0.0.2'
 
 setup(name='brukva',
       version=VERSION,
       description='Asynchronous Redis client that works within the Tornado IO loop',
-      author='Konstantin Merenkov',
-      author_email='kmerenkov@gmail.com',
+      maintainer='fzzbt',
+      maintainere_email='juhanipm@gmail.com',
       license='WTFPL',
-      url='http://github.com/kmerenkov/brukva',
+      url='http://github.com/fzzbt/brukva',
       keywords=['Redis', 'Tornado'],
       packages=['brukva'],
       test_suite='tests.all_tests',
